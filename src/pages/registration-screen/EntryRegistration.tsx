ce17885dde7e2b1e26115aaf6f343d548db0e986
moriokaR <Takashi.Morioka@jp.ricoh.com>
2024-01-29 16:03:27 +0900

    pageSizeエラー対処

--- src/pages/registration-screen/EntryRegistration.tsx
+++ src/pages/registration-screen/EntryRegistration.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { GetServerSideProps } from 'next';
-import { getVisitorInputInformation } from '../../information-processing/visitor-input-information';
-import { DataGrid } from '@mui/x-data-grid';
+import React from "react";
+import { GetServerSideProps } from "next";
+import { getVisitorInputInformation } from "../../information-processing/visitor-input-information";
+import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
-  { field: 'visitorID', headerName: 'visitorID', width: 70 },
-  { field: 'entryDateTime', headerName: 'entryDateTime', width: 200 },
-  { field: 'attender', headerName: 'attender', width: 200 },
-  { field: 'visitorName', headerName: 'visitorName', width: 200 },
-  { field: 'company', headerName: 'company', width: 200 },
+  { field: "visitorID", headerName: "visitorID", width: 70 },
+  { field: "entryDateTime", headerName: "entryDateTime", width: 200 },
+  { field: "attender", headerName: "attender", width: 200 },
+  { field: "visitorName", headerName: "visitorName", width: 200 },
+  { field: "company", headerName: "company", width: 200 },
 ];
 
 // VisitorData型の定義
@@ -25,8 +25,6 @@ interface HomePageProps {
 }
 
 const HomePage: React.FC<HomePageProps> = ({ initialData }) => {
-  console.log(initialData);
-
   return (
     <div>
       <h1>InputDateTime Example</h1>
@@ -34,24 +32,28 @@ const HomePage: React.FC<HomePageProps> = ({ initialData }) => {
       <div>
         {/* データの表示 */}
         <h2>来客者情報</h2>
-        
+
         <DataGrid
-  rows={initialData}
-  columns={columns}
-  // pageSize={5}
-  checkboxSelection
-  getRowId={(row) => row.visitorID}
-/>
-      
+          rows={initialData}
+          columns={columns}
+          initialState={{
+            pagination: { paginationModel: { pageSize: 5 } },
+          }}
+          pageSizeOptions={[5, 10, 25]}
+          checkboxSelection
+          getRowId={(row) => row.visitorID}
+        />
       </div>
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps<HomePageProps> = async () => {
+export const getServerSideProps: GetServerSideProps<
+  HomePageProps
+> = async () => {
   // APIからデータを取得
   const apiResponse = await getVisitorInputInformation();
-  
+
   // ApiResponse型からVisitorData[]型に変換
   const initialData: VisitorData[] = apiResponse.data;
 
