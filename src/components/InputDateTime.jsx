b97cda48f3577a6fc0a2d410ec6f786f6a54f992
moriokaR <Takashi.Morioka@jp.ricoh.com>
2024-02-13 17:42:44 +0900

    日時入力日本語化　css調整完成

--- src/components/InputDateTime.jsx
+++ src/components/InputDateTime.jsx
@@ -1,15 +1,33 @@
 // components/InputDateTime.js
 
 import React from "react";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import styles from "/src/styles/InputDateTime.module.css";
+import ja from "date-fns/locale/ja";
 
 /**
  * @param selectedDate - 選択された日付と時刻
  * @param onChange - 日付と時刻が変更されたときのコールバック関数
  */
 const InputDateTime = ({ selectedDate, onChange }) => {
+  registerLocale("ja", ja);
+
+  const renderCustomHeader = ({ date, decreaseMonth, increaseMonth }) => {
+    const year = new Intl.DateTimeFormat("ja", { year: "numeric" }).format(
+      date
+    );
+    const month = new Intl.DateTimeFormat("ja", { month: "long" }).format(date);
+
+    return (
+      <div className={styles.customHeader}>
+        <button onClick={decreaseMonth}>{`<`}</button>
+        <span>{`${year} ${month}`}</span>
+        <button onClick={increaseMonth}>{`>`}</button>
+      </div>
+    );
+  };
+
   return (
     <DatePicker
       selected={selectedDate}
@@ -17,10 +35,12 @@ const InputDateTime = ({ selectedDate, onChange }) => {
       showTimeSelect
       timeFormat="HH:mm"
       timeIntervals={15}
-      dateFormat="yyyy/MM/dd, hh:mm aa"
-      timeCaption="Time"
+      dateFormat="yyyy/MM/dd, HH:mm"
+      timeCaption="時間"
       customInput={<CustomInput />}
       calendarClassName={styles.customCalendar}
+      locale="ja"
+      renderCustomHeader={renderCustomHeader}
     />
   );
 };
